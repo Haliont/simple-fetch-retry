b5fd4291359d9058df245edd14d812efafa180cb
Konstantin <zemliakov.konstantin@gmail.com>
2019-10-31 22:53:16 +0600

    recursion, bro

--- index.ts
+++ index.ts
@@ -1,42 +1,27 @@
 interface RetryParams {
-    retries: number
-    retryDelay: number
+  retries?: number
+  retryDelay?: number
 }
 
-export function fetchRetry(url: string, retryParams: RetryParams, options = {}) {
-    // default retryParams
-    let retries = 3
-    let retryDelay = 800
+const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time));
 
-    // custom retryParams
-    if (retryParams && retryParams.retries) retries = retryParams.retries
-    if (retryParams && retryParams.retryDelay) retryDelay = retryParams.retryDelay
-    let counter = 0
+export function fetchRetry(url: string, retryParams: RetryParams = {}, options = {}) {
+  const { retries = 3, retryDelay = 800 } = retryParams
 
-    return new Promise(resolve => {
-        function callFetch() {
-            fetch(url, options)
-                .then(response => resolve(response))
-                .catch(async (error) => {
-                    if (counter < retries) {
-                        retry()
-                    } else {
-                        throw new Error(`Не удалось загрузить данные | ${error.message}`)
-                    }
-                })
-        }
+  return new Promise(resolve => {
+      function callFetch(attempts: number) {
+        fetch(url, options)
+          .then(resolve)
+          .catch(async (error) => {
+              if (attempts < retries) {
+                  await delay(retryDelay)
+                  callFetch(attempts + 1)
+              } else {
+                  throw new Error(`Не удалось загрузить данные | ${error.message}`)
+              }
+          })
+      }
 
-        callFetch()
-
-        function retry() {
-            setTimeout(() => {
-                counter++
-                callFetch()
-            }, retryDelay)
-        }
-    })
-
-    function delay(time: number) {
-        return new Promise(resolve => setTimeout(resolve, time));
-    }
+      callFetch(1)
+  })
 }
